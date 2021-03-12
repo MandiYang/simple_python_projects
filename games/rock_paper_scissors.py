1374afe08eeee75917ecf84092886cdaa005a203
MandiYang <64307609+MandiYang@users.noreply.github.com>
2021-03-12 20:38:51 +0100

    Update rock_paper_scissors.py

--- games/rock_paper_scissors.py
+++ games/rock_paper_scissors.py
@@ -1,36 +1,36 @@
-import random
-alternatives = ['rock', 'scissors', 'paper']
-play_again='yes'
+import random  #imports the random module to make random choices
+alternatives = ['rock', 'scissors', 'paper'] #Create a list of weapons
+play_again='yes' 
 while play_again=='yes':
     
     print('Welcome to my game, rock, scisscors and paper. \n' \
-      'Choose your weapon below')
-    answer = input('\n rock \n scissors \n paper \n: ')
+      'Choose your weapon below') 
+    answer = input('\n rock \n scissors \n paper \n: ') #Asking for weapons to use(answer)
     computer_ans = random.choice(alternatives)
     
-    if answer==computer_ans:
+    if answer==computer_ans:       #Define if it becomes tie
         print('computer:' + computer_ans + ' vs ' + 'you:' + answer)
         print('tie')
         
-    if (computer_ans=='rock' and answer=='scissors') \
+    if (computer_ans=='rock' and answer=='scissors') \  #Define if you loose
         or (computer_ans=='scissors' and answer=='paper') \
         or (computer_ans=='paper' and answer=='rock'):
         print('computer:' + computer_ans + ' vs ' + 'you:' + answer)
         print('you loose')
     
     
-    if (computer_ans=='scissors' and answer=='rock') \
+    if (computer_ans=='scissors' and answer=='rock') \ #Define if you win
         or (computer_ans=='paper' and answer=='scissors') \
         or (computer_ans=='rock' and answer=='paper'):
         print('computer:' + computer_ans + ' vs ' + 'you:' + answer)
         print('you win')
     
-    if (answer!='scissors') and (answer != 'rock') and (answer!='paper'):
+    if (answer!='scissors') and (answer != 'rock') and (answer!='paper'): #Define if your answer is not valid
         print('computer:' + computer_ans + ' vs ' + 'you:' + answer)
         print('That\'s not a valid answer')
         
     
-    play_again=input('play_again? yes=yes/no: ')
+    play_again=input('play_again? yes=yes/no: ')  #Ask to play agian(yes or no)
     
 
 
